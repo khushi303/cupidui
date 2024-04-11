699f982a5f7ae6f3b51f0281703573c5aff858aa
khushi303 <maankhushi303@gmail.com>
2024-04-11 10:31:43 +0530

    change in line height and zindex

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
         <>
             <div className='relative overflow-hidden'>
                 <div className='container mx-auto px-3 max-w-[1320px] lg:pt-32 lg:pb-40 md:py-32 py-10 relative z-20'>
-                    <h2 className='font-semibold sm:text-2xxl text-5xl leading-[46px] sm:leading-[65px] text-center text-white mb-6 relative'>Be part of the future of <span className='md:block'>IdentityHub</span><div className='absolute -bottom-3.5 left-0 right-0 flex items-center justify-center'><Gradientline /></div></h2>
-                    <p className='text-lightgray-600 font-normal text-xl leading-8 text-center'>Unleash the power of AI within cupid. Upgrade your productivity with <span className='md:block'>cupid, the open AI chat app.</span></p>
+                    <h2 className='font-semibold sm:text-2xxl text-5xl leading-[72px] sm:leading-[65px] text-center text-white mb-6 relative z-20'>Be part of the future of <span className='md:block'>IdentityHub</span><div className='absolute -bottom-3.5 left-0 right-0 flex items-center justify-center'><Gradientline /></div></h2>
+                    <p className='text-lightgray-600 font-normal text-xl leading-8 text-center relative z-20'>Unleash the power of AI within cupid. Upgrade your productivity with <span className='md:block'>cupid, the open AI chat app.</span></p>
                     <div className='flex items-center justify-center '>
                         <button className='mt-8 px-5 py-3 bg-btngradientfooter rounded-xl hover:shadow-gradientRed transition-all duration-300 ease-linear text-base font-medium text-white text-center relative z-10'>Start free trail</button>
                     </div>
-                    <div className='absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center z-[1]'><Pattern /></div>
+                    <div className='absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center z-[0]'><Pattern /></div>
                 </div>
                 <div className='lg:mt-20 md:mt-11 mt-5 lg:pt-2 container mx-auto px-3 max-w-[1320px] relative z-10'>
                     <div className='flex items-center lg:justify-between lg:flex-row flex-col border-b border-solid border-lightgray-700 lg:mt-10 lg:gap-0 gap-3 lg:pb-0 pb-5'>
